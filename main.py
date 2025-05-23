9735f0496601b6ba2783a0c4649a20adf79f4e66
Nolan Keim <nolobot1995@gmail.com>
2025-05-23 13:27:39 -0400

    Add basics to main file

---
+++ main.py
@@ -0,0 +1,10 @@
+varInt = 5
+varFloat = 5.0
+varString = "my string"
+# TODO: lists, tuples, dicts, bytes (all types), classes, etc.
+
+def main():
+    print("Printing variables")
+
+if __name__ == "__main__":
+    main()
