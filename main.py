cc2b99d3a5f470bbecaf203c3eacf8a86c486a9c
Nolan Keim <nolobot1995@gmail.com>
2025-05-24 12:34:46 -0400

    Add temp changes to main

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+
+# python numeric types
 varInt = 5
 varFloat = 5.0
+varComplex = 5.0
+
+# python string type
 varString = "my string"
+
+# python sequences
+varList =
+varTuple =
+varDictionary =
 # TODO: lists, tuples, dicts, bytes (all types), classes, etc.
 
 def main():
